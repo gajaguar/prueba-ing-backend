30bf270bab0e84a706a718e85f9b28dce4035b40
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-20 21:35:51 +0000

    🚧 chore(resources): add imports to `app.resources`
    - `BaseResource`
    - `levels`
    - `players`
    - `schemes`
    - `teams`

---
+++ app/resources/__init__.py
@@ -0,0 +1,5 @@
+from app.resources.base_resource import BaseResource
+from app.resources.levels_resource import levels
+from app.resources.players_resource import players
+from app.resources.schemes_resource import schemes
+from app.resources.teams_resource import teams

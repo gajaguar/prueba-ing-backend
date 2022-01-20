e93af65fd8e3776fc5724d919838c8b787aa9cb3
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-20 19:45:47 +0000

    🚧 chore(models) add imports to `app.models`
    - `Level`
    - `Player`
    - `Scheme`
    - `Team`

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["Level", "Player", "Scheme", "Team"]
+from app.models.models import Level
+from app.models.models import Player
+from app.models.models import Scheme
+from app.models.models import Team

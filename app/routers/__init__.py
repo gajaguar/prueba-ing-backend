9cacf9bcf970b3ef4f40509574ead9db3fd10595
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-22 01:42:27 +0000

    🚧 chore(routers): add imports to `app.routers`
    - `levels_router`
    - `players_router`
    - `schemes_router`
    - `teams_router`

---
+++ app/routers/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["levels_router", "players_router", "schemes_router", "teams_router"]
+from app.routers.levels_router import levels_router
+from app.routers.players_router import players_router
+from app.routers.schemes_router import schemes_router
+from app.routers.teams_router import teams_router

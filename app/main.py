fd5e3357cab63d7eaebc2ad4c33a99d5e5b765b3
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-22 02:09:09 +0000

    🚧 chore(main): add startup function
    On `startup` event initialize the database.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import uvicorn
 
 from fastapi import FastAPI
 
+from app.database import init_db
 from app.routers import *
 
 
@@ -12,8 +13,18 @@ app.include_router(players_router)
 app.include_router(schemes_router)
 app.include_router(teams_router)
 
+@app.on_event("startup")
+def startup():
+    """
+    Initialize the database if this option is setted in environment variables.
+    """
+    init_db()
+
 @app.get("/")
 def root():
+    """
+    Welcome page.
+    """
     return {"hello": "world"}
     
 if __name__ == "__main__":

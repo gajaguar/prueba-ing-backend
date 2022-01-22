276e0c1dc4b20e35d705f336b9ae81e45f4047b8
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-22 02:29:59 +0000

    🚧 chore(factories): add import to `app.factories`
    - `RandomLevelFactory`
    - `RandomPlayerFactory`
    - `RandomSchemeFactory`
    - `RandomTeamFactory`

---
+++ app/factories/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "RandomLevelFactory",
+    "RandomPlayerFactory",
+    "RandomSchemeFactory",
+    "RandomTeamFactory"
+]
+from app.factories.level_factories import RandomLevelFactory
+from app.factories.players_factories import RandomPlayerFactory
+from app.factories.scheme_factories import RandomSchemeFactory
+from app.factories.team_factories import RandomTeamFactory

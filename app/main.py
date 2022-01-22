d345c006ce9a929991a1446a61ceefe57a5e6f57
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-22 01:50:17 +0000

    🚧 chore(main): add routers
    - `levels_router`
    - `players_router`
    - `schemes_router`
    - `teams_router`

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ import uvicorn
 
 from fastapi import FastAPI
 
+from app.routers import *
+
 
 app = FastAPI()
+
+app.include_router(levels_router)
+app.include_router(players_router)
+app.include_router(schemes_router)
+app.include_router(teams_router)
 
 @app.get("/")
 def root():

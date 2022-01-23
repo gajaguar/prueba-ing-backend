98c6b778c0c15ba61b941f210f2f5c25d8a208a0
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-23 01:02:27 +0000

    🐛 fix(database): add `session` to resource methods.
    Also add log message.

--- app/database/populate.py
+++ app/database/populate.py
@@ -1,3 +1,6 @@
+from sqlmodel import Session
+
+from app.database import engine
 from app.factories import *
 from app.resources import *
 
@@ -6,31 +9,38 @@ def populate_schemes(quantity: int)-> None:
     """
     Populate the database with specified quantity of schemes.
     """
+    session = Session(engine)
     random_schemes = RandomSchemeFactory.create_batch(quantity)
     for scheme in random_schemes:
-        schemes.create(scheme)
-    print("DEBUG:   ", f"{quantity} random schemes created.")
+        schemes.create(session, scheme)
+    print("DATABASE:", f"{quantity} random schemes created.")
 
 def populate_levels(quantity: int)-> None:
     """
     Populate the database with specified quantity of levels.
     """
+    session = Session(engine)
     random_levels = RandomLevelFactory.create_batch(quantity)
     for level in random_levels:
-        levels.create(level)
+        levels.create(session, level)
+    print("DATABASE:", f"{quantity} random levels created.")
 
 def populate_teams(quantity: int)-> None:
     """
     Populate the database with specified quantity of teams.
     """
+    session = Session(engine)
     random_teams = RandomTeamFactory.create_batch(quantity)
     for team in random_teams:
-        teams.create(team)
+        teams.create(session, team)
+    print("DATABASE:", f"{quantity} random teams created.")
 
 def populate_players(quantity: int)-> None:
     """
     Populate the database with specified quantity of players.
     """
+    session = Session(engine)
     random_players = RandomPlayerFactory.create_batch(quantity)
     for player in random_players:
-        players.create(player)
+        players.create(session, player)
+    print("DATABASE:", f"{quantity} random players created.")

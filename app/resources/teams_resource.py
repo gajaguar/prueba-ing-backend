30f05a476ff7c2420c3c0fc72a1c5763a2f9595f
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-23 06:27:37 +0000

    ✨ feat(resources): implement method in `TeamsResource`
    - `get_team_goals_ratio`

--- app/resources/teams_resource.py
+++ app/resources/teams_resource.py
@@ -1,13 +1,34 @@
+from sqlmodel import Session
+
 from app.models import Team
 from app.resources.base_resource import BaseResource
+from app.responses import NotFoundException
 from app.schemas import TeamCreate, TeamUpdate
+from app.utils import get_ratio
 
 
 class TeamsResource(BaseResource[Team, TeamCreate, TeamUpdate]):
     """
     Class representing a teams resource.
     """
-    pass
+    def get_team_goals_ratio(self, db: Session, id: int) -> float:
+        """
+        Obtain the team goals ratio of a team.
+        """
+        team = db.get(self.model, id)
+        if team == None:
+            raise NotFoundException
+        reached_goals = 0
+        required_goals = 0
+        players = team.players
+        for player in players:
+            reached_goals += player.player_goals
+            required_goals += player.level.level_goals
+        if reached_goals >= required_goals:
+            return 1
+        else:
+            team_goals_ratio = get_ratio(reached_goals, required_goals)
+            return team_goals_ratio
 
 
 teams = TeamsResource(Team)

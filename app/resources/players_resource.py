0eeec992212854bf892ca820ad6b96c4f459dfb7
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-23 06:28:24 +0000

    ✨ feat(resources): implement methods in `PlayersResource`
    - `get_individual_goals_ratio`
    - `get_bonus_salary`
    
    And overwrite method:
    
    - `get_one`

--- app/resources/players_resource.py
+++ app/resources/players_resource.py
@@ -1,13 +1,61 @@
+from sqlmodel import Session
+
 from app.models import Player
 from app.resources.base_resource import BaseResource
+from app.resources.teams_resource import teams
+from app.responses import NotFoundException
 from app.schemas import PlayerCreate, PlayerUpdate
+from app.utils import calculate_bonus
+from app.utils import get_ratio
 
 
 class PlayersResource(BaseResource[Player, PlayerCreate, PlayerUpdate]):
     """
-    Class representing a roles resource.
+    Class representing a players resource.
     """
-    pass
+    def get_individual_goals_ratio(self, db: Session, id: int) -> float:
+        """
+        Obtain the individual goals ratio of a player.
+        """
+        player = db.get(self.model, id)
+        if player == None:
+            raise NotFoundException
+        reached_goals = player.player_goals
+        required_goals = player.level.level_goals
+        if reached_goals >= required_goals:
+            return 1
+        else:
+            individual_goals_ratio = get_ratio(reached_goals, required_goals)
+            return individual_goals_ratio
 
+    def get_bonus_salary(self, db: Session, id: int) -> int:
+        """
+        Obtain the bonus salary of a player.
+        """
+        player = db.get(self.model, id)
+        if player == None:
+            raise NotFoundException
+        individual_goals_ratio = self.get_individual_goals_ratio(db, id)
+        team_goals_ratio = teams.get_team_goals_ratio(db, player.team_id)
+        calculated_bonus = calculate_bonus(
+            player.player_bonus_salary,
+            individual_goals_ratio,
+            team_goals_ratio
+        )
+        return calculated_bonus
+
+    def get_one(self, db: Session, id: int) -> Player:
+        """
+        Obtain a single player from the database and calculate its integrated
+        salary.
+        """
+        player = db.get(self.model, id)
+        if player == None:
+            raise NotFoundException
+        bonus_salary = self.get_bonus_salary(db, id)
+        integrated_salary = player.player_base_salary + bonus_salary
+        player.player_integrated_salary = integrated_salary
+        return player
+ 
 
 players = PlayersResource(Player)

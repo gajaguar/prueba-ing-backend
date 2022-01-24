9176e1cb086272c4a5289db19c2f07d6182b466d
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-24 14:59:14 -0600

    🚧 chore(schemas): add import to `app.schemas`
    - `PlayerCreateBatch`

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -3,6 +3,7 @@ from app.schemas.level_schemas import LevelCreate
 from app.schemas.level_schemas import LevelUpdate
 from app.schemas.player_schemas import Player
 from app.schemas.player_schemas import PlayerCreate
+from app.schemas.player_schemas import PlayerCreateBatch
 from app.schemas.player_schemas import PlayerUpdate
 from app.schemas.scheme_schemas import Scheme
 from app.schemas.scheme_schemas import SchemeCreate

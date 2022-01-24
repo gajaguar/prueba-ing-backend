19444c4c0206781f9a6a4be4b8aa395fe2809396
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-24 16:38:10 -0600

    ✨ chore(routers): improve `players_router`
    Implement path operation: `create_players`.

--- app/routers/players_router.py
+++ app/routers/players_router.py
@@ -1,6 +1,15 @@
+from typing import List
+
+from fastapi import Depends, status
+from sqlmodel import Session
+
+from app.dependencies import get_session
 from app.resources import players
-from app.schemas import Player, PlayerCreate, PlayerUpdate
 from app.routers.base_router import setup_router
+from app.schemas import Player
+from app.schemas import PlayerCreate
+from app.schemas import PlayerCreateBatch
+from app.schemas import PlayerUpdate
 
 
 players_router = setup_router(
@@ -10,3 +19,18 @@ players_router = setup_router(
     PlayerCreate,
     PlayerUpdate
 )
+
+@players_router.post(
+    "/batch",
+    status_code=status.HTTP_201_CREATED,
+    response_model=List[Player],
+)
+async def create_players(
+    data: PlayerCreateBatch,
+    session: Session = Depends(get_session),
+):
+    """
+    Create multiple players in the database.
+    """
+    created = players.create_by_batch(session, data)
+    return created
